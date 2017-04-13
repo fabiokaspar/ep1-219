586d69d5bd0b1f9a853984061d8e20b46580f8f6
fabiokaspar <fabiokaspar@gmail.com>
2017-04-13 20:43:37 -0300

    roda medidas implementado; gera graficos no python quase pronto.

---
+++ src/run_measurements.sh
@@ -0,0 +1,72 @@
+#! /bin/bash
+
+set -o xtrace
+
+#ITERATIONS_SIZE=10
+
+
+ITERATIONS_SIZE=5
+INITIAL_SIZE=16
+
+NTHREADS_MAX=32
+MEASUREMENTS=10
+
+SIZE=$INITIAL_SIZE
+
+NAMES=('mandelbrot_seq' 'mandelbrot_pth' 'mandelbrot_omp')
+MODOS=('com' 'sem')
+
+
+make
+mkdir results
+
+for NAME in ${NAMES[@]}; do
+    mkdir results/$NAME
+
+    if [ $NAME = 'mandelbrot_seq' ] 
+    then    
+        for MODO in ${MODOS[@]}; do 
+            for ((i = 1; i <= $ITERATIONS_SIZE; i++)); do
+                for ((j = 1; j <= MEASUREMENTS; j++)); do
+
+                    perf stat -r 1 ./$NAME -2.5 1.5 -2.0 2.0 $SIZE $MODO >> "full_$MODO.log" 2>&1
+                    #perf stat -r $MEASUREMENTS ./$NAME -0.8 -0.7 0.05 0.15 $SIZE >> seahorse.log 2>&1
+                    #perf stat -r $MEASUREMENTS ./$NAME 0.175 0.375 -0.1 0.1 $SIZE >> elephant.log 2>&1
+                    #perf stat -r $MEASUREMENTS ./$NAME -0.188 -0.012 0.554 0.754 $SIZE >> triple_spiral.log 2>&1
+                done
+                
+                SIZE=$(($SIZE * 2))
+            done
+
+            SIZE=$INITIAL_SIZE
+            mv *.log results/$NAME
+       done
+
+    else
+        for ((NT = 1; $NT <= $NTHREADS_MAX; NT = $(($NT * 2)) )); do
+
+            for ((i = 1; i <= $ITERATIONS_SIZE; i++)); do
+                for ((j = 1; j <= MEASUREMENTS; j++)); do
+
+                    perf stat -r 1 ./$NAME -2.5 1.5 -2.0 2.0 $SIZE $NT >> 'full_'$NT'.log' 2>&1
+                    #perf stat -r $MEASUREMENTS ./$NAME -0.8 -0.7 0.05 0.15 $SIZE >> seahorse.log 2>&1
+                    #perf stat -r $MEASUREMENTS ./$NAME 0.175 0.375 -0.1 0.1 $SIZE >> elephant.log 2>&1
+                    #perf stat -r $MEASUREMENTS ./$NAME -0.188 -0.012 0.554 0.754 $SIZE >> triple_spiral.log 2>&1
+                done
+                
+                SIZE=$(($SIZE * 2))
+            done
+
+            SIZE=$INITIAL_SIZE
+            mv *.log results/$NAME
+        done
+    fi
+
+done
+
+#rm output.ppm
+
+
+
+
+
