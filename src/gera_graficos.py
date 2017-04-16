dc4a3f0bc1afef5608b8221becd18817713d9d5b
fabiokaspar <fabiokaspar@gmail.com>
2017-04-16 14:19:52 -0300

    média e desvio padrão implementados e aplicado sobre os dados dos graficos. Também imprime tabelas com estatísticas no mesmo arquivo que os boxplots, mas em partes separadas do arquivo. Agora, a geração de gráficos e estatísticas dos mesmos está finalizada, faltando somente a análise científica dos dados finais a serem coletados.

--- src/gera_graficos.py
+++ src/gera_graficos.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
+# coding: utf8
 
 # Referencia de como fazer boxplot: 
 # http://blog.bharatbhole.com/creating-boxplots-with-matplotlib/
 
-#import numpy as np
+from __future__ import unicode_literals
+
+import numpy as np
 import matplotlib as mpl 
 import re
 import os
@@ -11,7 +14,7 @@ import os
 mpl.use('agg') ## agg backend is used to create plot as a .png file
 import matplotlib.pyplot as plt 
 
-
+import statistics as stat
 
 #id_fig = 0
 num_medicoes = 10
@@ -21,6 +24,7 @@ regioes = ['full', 'seahorse', 'elephant', 'triple_spiral']
 
 
 entradas = ['16']
+rotulos = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
 
 # calcula todos os tamanhos de entrada, mas como string
 while len(entradas) < num_entradas:
@@ -45,11 +49,8 @@ for diretorio in lista_diretorios:
 
 		# debug
 		print diretorio+"/"+arquivo
-		print numeros
-		print len(numeros)
-		print type(numeros)
+		
 
-		print "**************\n"
 
 		arq.close()
 
@@ -59,39 +60,61 @@ for diretorio in lista_diretorios:
 		#print numeros
 			
 		data_to_plot = []
+		media = []
+		mediana = []
+		dp = []
 
 		for j in range(num_entradas):
-			## Cria uma colecao
-			box_plot = numeros[num_medicoes * j : num_medicoes * (j+1)]
-			
-			##  E a adiciona numa lista  
-			data_to_plot.append(box_plot)
-			
+			## define uma amostra
+			amostra = numeros[num_medicoes * j : num_medicoes * (j+1)]
+			amostra.sort()
 
+			media.append(stat.mean(amostra))
+			mediana.append(stat.median(amostra))
+			dp.append(stat.stdev(amostra))
 
-		# Cria uma instancia de figura
-		#fig = plt.figure(id_fig)
-		#id_fig = id_fig+1
+			##  E a adiciona numa coleção global delas  
+			data_to_plot.append(amostra)
+			print amostra
+			print "**************\n"
 
-		# Cria uma instancia de eixos
-		#ax = fig.add_subplot(111)
-
-
-		fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(9, 9))
+		
+		
+		# Cria uma instancia de figura e eixos
+		fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(10, 9))
 
 		# Cria o boxplot
-		#boxplot = ax.boxplot(data_to_plot, patch_artist=True)
-		boxplot = ax.boxplot(data_to_plot)
+		box_plot = ax.boxplot(data_to_plot)
 
-		#for patch in boxplot['boxes']:
-		#	patch.set_facecolor('lightgreen')
+		id_box = 0
+		for line in box_plot['boxes']:
+			x, y = line.get_xydata()[0]
+			plt.text(x+0.1, y+0.008, rotulos[id_box], color='red', fontsize=11, horizontalalignment='right', verticalalignment='top')
+			id_box += 1
 
+		col_labels = ['média', 'mediana', 'desvio padrão', '< valor obs.', '> valor obs.']
+		row_labels = []
+		table_vals = []
+
+		for j in range(num_entradas):
+			table_vals.append([ media[j], mediana[j], dp[j], data_to_plot[j][0], data_to_plot[j][-1] ])
+			row_labels.append('box '+ rotulos[j])
+
+
+		the_table = plt.table(cellText=table_vals,
+                  colWidths = [0.1] * 5,
+                  rowLabels=row_labels,
+                  colLabels=col_labels,
+                  bbox=[0, 1.2, 0.8, 0.3],
+                  fontsize=24)
+
+		the_table.scale(1.8, 1.8)
+		
 		ax.set_xticklabels(entradas)
 		ax.yaxis.grid(True)
 		
 		plt.xlabel('Tamanho Da Entrada', fontsize=10, color='red')
 		plt.ylabel('Tempo Em Segundos', fontsize=10, color='red')
-
 
 		title = './'+diretorio
 		title += ', regiao: '
@@ -102,7 +125,6 @@ for diretorio in lista_diretorios:
 			if len(lista) > 0:
 				title += regiao
 
-		
 		if diretorio == 'mandelbrot_seq':
 			s = arquivo[-7:-4]
 			title += (', '+s.upper()+' I/O e aloc. memoria')
@@ -117,8 +139,7 @@ for diretorio in lista_diretorios:
 		## Remove top axes and right axes ticks
 		ax.get_xaxis().tick_bottom()
 		ax.get_yaxis().tick_left()
-
+		plt.yticks(np.arange(0, 0.165, 0.005))
 		
 		# Salva a figura
 		fig.savefig('graphics/'+diretorio+'/'+arquivo[0:-4]+'.png', bbox_inches='tight')
-
---
+++ src/teste.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import statistics as stat
+
+
+data = [0, -1, 3, 2, 4, 1, 0, 5.5, 6.777]
+
+data.sort()
+
+print data
+
+
+media = stat.mean(data)
+mediana = stat.median(data)
+variancia = stat.variance(data)
+dp = stat.stdev(data)
+
+print "media = ", media
+print "mediana = ", mediana
+print "variancia = ", variancia
+print "desvio padrao = ", dp
+
